2e33cdf91924de48ab4237ed0a4d3d2494d17f78
zhanggx <enfizgx@163.com>
2019-11-20 23:29:22 +0800

    add base template and error template

--- app.py
+++ app.py
@@ -18,6 +18,10 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # 关闭对模型修改的
 # 在扩展类实例化前加载配置
 db = SQLAlchemy(app)
 
+@app.context_processor
+def inject_user():
+    user = User.query.first()
+    return dict(user=user)
 
 class User(db.Model):  # 表名将会是 user（自动生成，小写处理）
     id = db.Column(db.Integer, primary_key=True)  # 主键
@@ -31,9 +35,12 @@ class Movie(db.Model):  # 表名将会是 movie
 
 @app.route('/')
 def index():
-    user =User.query.first()
     movies = Movie.query.all()
-    return render_template('index.html',user=user,movies=movies)
+    return render_template('index.html',movies=movies)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'),404
 
 @app.cli.command() #注册为命令
 @click.option('--drop',is_flag=True,help='create after drop.')
